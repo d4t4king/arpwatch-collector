cf72c02556eb7d7155901f243388b569a8e729fa
dataking <dataking@gmail.com>
2017-09-09 00:00:17 -0700

    WIP on perl

--- collector.pl
+++ collector.pl
@@ -3,7 +3,22 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 sub process_dat {
 	my $blob = shift;
-	my ($mac, $ip, $date, $name, $iface) = split(/\s+/, $blob);
+	my @lines = split(/\r?\n/, $blob);
+	foreach my $l ( @lines ) {
+		my ($mac, $ip, $epoch, $name, $iface) = split(/\t/, $l);
+		print "$mac, $ip, $epoch, $name, $iface\n";
+	}
 }
+
+{
+	local $/ = undef;
+	open IN, "</var/lib/arpwatch/eth1.dat" or die "Couldn't open arpwatch data file: $!";
+	my $blob = <IN>;
+	close IN or die "There was a problem closing the arpwatch data file: $!";
+	&process_dat($blob);
+}
+
