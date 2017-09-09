f195027aeeacd5b8288f9757e6204f0cedf41c23
dataking <dataking@gmail.com>
2017-09-08 20:09:54 -0700

    WIP

--- collector.py
+++ collector.py
@@ -2,9 +2,26 @@
 
 import argparse
 import pprint
+import os.path
+import paramiko
+
+pp = pprint.PrettyPrinter(indent=4)
 
 args_parse = argparse.ArgumentParser(description="Get those ARPs!")
 args_parse.add_argument('-t', '--target-file', dest='target_file', help="The list of end points to colect from")
 args = args_parse.parse_args()
 
+if os.path.isfile(args.target_file):
+	with open(args.target_file, 'r') as f:
+		lines = f.read().splitlines()
 
+# prepare the SSH client
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+for h in lines:
+	h.lstrip()
+	h.rstrip()
+	client.connect(h, username='root', key_filename='/root/.ssh/id_rsa')
+	pp.pprint(client)
+	break
