6f2fd75dd8423177723da6775912a56b2ff22b1a
dataking <dataking@gmail.com>
2017-09-20 22:38:32 -0700

    report.py WIP

---
+++ report.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import pprint
+import sqlite3
+import argparse
+import datetime
+
+pp = pprint.PrettyPrinter(indent=4)
+
+args_parse = argparse.ArgumentParser(description="Get those ARPs!")
+args_parse.add_argument('-d', '--dbfile', dest='dbfile', help='The database file to read the collected data from.')
+args_parse.add_argument('-r', '--report', dest='report_type', help='The type of report to run.  Available options are: agent-summary.')
+
+def execute_atomic_int_query(sql):
+	conn = sqlite3.connect(args.dbfile)
+	with conn:
+		cur = conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+		result = cur.fetchone()
+		my_int = 0
+		if 'tuple' in str(type(result)):
+			my_int = result[0]
+		elif 'int' in str(type(rsult)):
+			my_int = result
+		elif 'NoneType' in str(type(result)):
+			my_int = False
+		else:
+			raise TypeError("Unexpected SQL query result type: " + str(type(result)) + ".")
+		return my_int
+
+def execute_single_row_query(sql):
+	conn = sqlite3.connect(args.dbfile)
+	with conn:
+		cur = conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+		result = cur.fetchone()
