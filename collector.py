fd779b5b9c4d4ca9aa3ffe0d53a8d2fbbd5fbe79
dataking <dataking@gmail.com>
2017-10-21 17:20:13 -0700

    name resolution print work

--- collector.py
+++ collector.py
@@ -140,7 +140,11 @@ def main():
                             except dns.resolver.NXDOMAIN, nx:
                                 a = 'UNRESOLVED'
                             if a:
-                                execute_non_query("INSERT INTO agents (ipaddr,fqdn,first_pull_date,last_update) VALUES ('" + agnt + "','" + str(a) + "','" + str(epoch_now.strftime('%s')) + "','" + str(epoch_now.strftime('%s')) + "')")
+                                #pp.pprint(a.rrset)
+                                #print dir(a.rrset)
+                                print a.rrset.name
+                                #exit(1)
+                                execute_non_query("INSERT INTO agents (ipaddr,fqdn,first_pull_date,last_update) VALUES ('" + agnt + "','" + str(a.rrset.name) + "','" + str(epoch_now.strftime('%s')) + "','" + str(epoch_now.strftime('%s')) + "')")
                             else:
                                 execute_non_query("INSERT INTO agents (ipaddr,first_pull_date,last_update) VALUES ('" + agnt + "','" + str(epoch_now.strftime('%s')) + "','" + str(epoch_now.strftime('%s')) + "')")
                         else:                                                   # assume it looks like an FQDN
