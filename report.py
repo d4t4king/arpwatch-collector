19935dd1f882a00593cafb6c61d6d64d0c473c30
dataking <dataking@gmail.com>
2017-11-05 14:46:15 -0800

    adding client-detail report; fixed some formatting

--- report.py
+++ report.py
@@ -68,7 +68,7 @@ def get_oldest(what):
             if oldest_date == 0 or oldest_date > dateint:
                 oldest_date = dateint
                 oldest_i = c[0]
-        old_obj = execute_single_row_query("SELECT h.id,m.mac_addr,ip.ipaddr FROM hosts h INNER JOIN macs m ON h.mac_id=m.id INNER JOIN ipaddrs ip ON h.ipaddr_id=ip.id WHERE h.id='{0}'".format(oldest_i))
+        old_obj = execute_single_row_query("SELECT h.id,m.mac_addr,ip.ipaddr,h.name FROM hosts h INNER JOIN macs m ON h.mac_id=m.id INNER JOIN ipaddrs ip ON h.ipaddr_id=ip.id WHERE h.id='{0}'".format(oldest_i))
     else:
         raise Exception("Unrecognized entity ({0})".format(what))
     oldest_d[oldest_date] = old_obj
@@ -93,7 +93,7 @@ def get_newest(what):
             if newest_date == 0 or newest_date < dateint:
                 newest_date = dateint
                 newest_i = c[0]
-        new_obj = execute_single_row_query("SELECT h.id,m.mac_addr,ip.ipaddr FROM hosts h INNER JOIN macs m ON h.mac_id=m.id INNER JOIN ipaddrs ip ON h.ipaddr_id=ip.id WHERE h.id='{0}'".format(newest_i))
+        new_obj = execute_single_row_query("SELECT h.id,m.mac_addr,ip.ipaddr,h.name FROM hosts h INNER JOIN macs m ON h.mac_id=m.id INNER JOIN ipaddrs ip ON h.ipaddr_id=ip.id WHERE h.id='{0}'".format(newest_i))
     else:
         raise Exception("Unrecognized entity ({0})".format(what))
     newest_d[newest_date] = new_obj
@@ -130,7 +130,7 @@ def main():
         print "| %17s | %-25s %23s|" % ("First seen: ", time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(dateint)), " ")
         last_updated = execute_atomic_int_query("SELECT last_update FROM agents WHERE id='{0}'".format(oldest[dateint][0]))
         print "| %17s | %-25s %23s|" % ("Last updated: ", time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(last_updated)), " ")
-        print '------------------------------------------------------------------------'
+        print '-' * 72
         newest = get_newest('agent')
         dateint = newest.keys()[0]
         if newest[dateint][2] and not newest[dateint][2] == 'None':
@@ -140,27 +140,37 @@ def main():
         print "| %17s | %-25s %23s|" % ("First seen: ", time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(dateint)), "")
         last_updated = execute_atomic_int_query("SELECT last_update FROM agents WHERE id='{0}'".format(newest[dateint][0]))
         print "| %17s | %-25s %23s|" % ("Last updated: ", time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(last_updated)), " ")
-        print "========================================================================"
+        print "=" * 72
     elif args.report_type == 'client-summary':
         total_macs = execute_atomic_int_query("SELECT COUNT(DISTINCT mac_addr) FROM macs")
         total_ips = execute_atomic_int_query("SELECT COUNT(DISTINCT ipaddr) FROM ipaddrs")
-        print "========================================================================"
+        print "=" * 72
         print "| Total unique MAC Addresses: %-41s|" % (total_macs)
-        print '------------------------------------------------------------------------'
+        print '-' * 72
         print "| Total unique IP addresses: %-42s|" % (total_ips) 
-        print "========================================================================"
+        print "=" * 72
         oldest = get_oldest('client')
         dateint = oldest.keys()[0]
-        print "| Oldest Client: %54s|" % " "
+        print "| Oldest Client: %-54s|" % oldest[dateint][3]
         print "| %-10s %17s %-10s %-29s|" % ("MAC Addr:", oldest[dateint][1], "IP Addr:", oldest[dateint][2])
         print "| %-17s %-50s |" % ("Date Discovered: ", time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(dateint)))
-        print '------------------------------------------------------------------------'
+        print '-' * 72
         newest = get_newest('client')
         dateint = newest.keys()[0]
-        print "| Newest Client: %54s|" % " "
+        print "| Newest Client: %-54s|" % newest[dateint][3]
         print "| %-10s %17s %-10s %-29s|" % ("MAC Addr:", newest[dateint][1], "IP Addr:", newest[dateint][2])
         print "| %-17s %-50s |" % ("Date Discovered: ", time.strftime('%m/%d/%y %H:%M:%S', time.localtime(dateint)))
-        print "========================================================================"
+        print "=" * 72
+    elif args.report_type == 'client-detail':
+        clients = execute_multi_row_query("SELECT id FROM hosts ORDER BY name")
+        print "=" * 105
+        print "| %-17s | %-17s | %-17s | %-19s | %-19s |" % ("Name","MAC Addr","IP Addr","Date Discovered","Last Updated")
+        for hid in clients:
+            #pp.pprint(hid)
+            ctup = execute_single_row_query("SELECT h.name,m.mac_addr,ip.ipaddr,h.date_discovered,h.last_updated FROM hosts h INNER JOIN macs m ON h.mac_id=m.id INNER JOIN ipaddrs ip ON h.ipaddr_id=ip.id WHERE h.id='{0}'".format(hid[0]))
+            #pp.pprint(ctup)
+            print("| %-17s | %-17s | %-17s | %-19s | %-19s |" % (ctup[0],ctup[1],ctup[2],time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(ctup[3])),time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(ctup[4]))))
+        print "=" * 105
     else:
         raise Exception("Unrecognized report type! ({0})".format(args.report_type))
 
